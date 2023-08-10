9368635fe84429367cdaa3e47c39e94784006d68
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2023-08-10 23:05:27 +0800

    Add project files.

---
+++ CMakeVcpkgManifest.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeVcpkgManifest.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeVcpkgManifest.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
