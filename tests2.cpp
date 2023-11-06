aac7dd81c4cfd9632a0c5cdb2b31dbb92e41981c
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2023-11-06 20:08:58 +0800

    test label

---
+++ tests2.cpp
@@ -0,0 +1,23 @@
+﻿// CMakeVcpkgManifest.cpp : Defines the entry point for the application.
+//
+
+#include <gtest/gtest.h>
+using namespace std;
+// Tests factorial of 0.
+
+int Factorial(int n) {
+  return n  ;
+}
+
+TEST(FactorialTest, Handles) {
+  EXPECT_EQ(Factorial(5), 5);
+}
+
+// Tests factorial of positive numbers.
+TEST(FactorialTest, HandlesPositiveInput) {
+  EXPECT_EQ(Factorial(1), 1);
+  EXPECT_EQ(Factorial(2), 2);
+  EXPECT_EQ(Factorial(3), 3);
+  EXPECT_EQ(Factorial(8), 8);
+}
+
